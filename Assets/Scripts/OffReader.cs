16e6930ca36501cb55f08a5dd6c27650aa2d3100
Andy Baker <andy@andybak.net>
2022-03-14 17:36:25 +0000

    A test scene showing .off file support

---
+++ Assets/Scripts/OffReader.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using Polyhydra.Core;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class OffReader : MonoBehaviour
+{
+    public string filename = "test.off";
+
+    [ContextMenu("Go")]
+    public void Go()
+    {
+        using (StreamReader reader = new StreamReader($"Assets/{filename}"))
+        {
+            var poly = new PolyMesh(reader);
+            var mesh = poly.BuildUnityMesh(colorMethod: PolyMesh.ColorMethods.ByTags);
+            gameObject.GetComponent<MeshFilter>().mesh = mesh;
+        }
+    }
+
+    private void OnValidate()
+    {
+        Go();
+    }
+}
