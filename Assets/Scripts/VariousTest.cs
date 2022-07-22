e8f9e7c48d7153b5932b8e6905602134c3ac76a3
Andy Baker <andy@andybak.net>
2022-07-22 08:33:41 +0100

    The common "Build" method for various solids was more trouble than it was worth.

--- Assets/Scripts/VariousTest.cs
+++ Assets/Scripts/VariousTest.cs
@@ -8,12 +8,47 @@ public class VariousTest : TestBase
     [Header("Base Shape Parameters")]
     public VariousSolidTypes type;
     [Range(1, 64)] public int X = 3;
-    [Range(1, 64)] public int Y = 3;
-    [Range(1, 64)] public int Z = 3;
-    
+    [Range(.01f, 64f)] public float Y = 3;
+    [Range(.01f, 64f)] public float Z = 3;
+
     public override void Go()
     {
-        poly = VariousSolids.Build(type, X, Y, Z);
+        switch (type)
+        {
+            case VariousSolidTypes.UvSphere:
+                poly = VariousSolids.UvSphere(
+                    X,
+                    Mathf.Max(Mathf.FloorToInt(Y), 1)
+                );
+                break;
+            case VariousSolidTypes.UvHemisphere:
+                poly = VariousSolids.UvHemisphere(
+                    X,
+                    Mathf.Max(Mathf.FloorToInt(Y), 1)
+                );
+                break;
+            case VariousSolidTypes.Stairs:
+                poly = VariousSolids.Stairs(
+                    X,
+                    Y,
+                    Z
+                );
+                break;
+            case VariousSolidTypes.Torus:
+                poly = VariousSolids.Torus(
+                    X,
+                    Mathf.Max(Mathf.FloorToInt(Y), 1),
+                    Z
+                );
+                break;
+            case VariousSolidTypes.Box:
+                poly = VariousSolids.Box(
+                    X,
+                    Mathf.Max(Mathf.FloorToInt(Y), 1),
+                    Mathf.Max(Mathf.FloorToInt(Z), 1)
+                );
+                break;
+        }
         Build();
     }
 }
