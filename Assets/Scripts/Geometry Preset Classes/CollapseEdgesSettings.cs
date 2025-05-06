4ed21c7685061585e0f2ca9da6f19351ff49a0b2
Andy Baker <andy@andybak.net>
2025-05-06 09:08:39 +0100

    Add support for filters to CollapseEdgeSettings

--- Assets/Scripts/Geometry Preset Classes/CollapseEdgesSettings.cs
+++ Assets/Scripts/Geometry Preset Classes/CollapseEdgesSettings.cs
@@ -1,21 +1,36 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Polyhydra.Core;
 using UnityEngine;
 using UnityEngine.Serialization;
+
+[Serializable]
+public class CollapseEdgeParameters
+{
+    public bool enabled = true;
+    [FormerlySerializedAs("Face1")] [Range(0,24)] public int Face1Sides = 3;
+    public Roles Face1Role;
+    [FormerlySerializedAs("Face2")] [Range(0,24)] public int Face2Sides = 4;
+    public bool Either = false;
+}
 
 [CreateAssetMenu(fileName = "CollapseEdgesSettings", menuName = "Polyhydra/CollapseEdgesSettings", order = 1)]
 public class CollapseEdgesSettings : GridSettings
 {
 
     [Header("Collapse Edge Parameters")]
-    [Range(0,24)] public int Face1 = 3;
-    [Range(0,24)] public int Face2 = 4;
-    public bool Either = false;
+    public List<CollapseEdgeParameters> CollapseEdgeParameterList;
 
     public override PolyMesh ModifyPostOp(PolyMesh poly)
     {
-        poly = poly.CollapseEdges(Face1, Face2, Either);
-        poly.DebugVerts = poly.Vertices.Select(v => v.Position).ToList();
+        foreach (var item in CollapseEdgeParameterList)
+        {
+            if (!item.enabled) continue;
+            var f = Filter.Role(item.Face1Role);
+            poly = poly.CollapseEdges(item.Face1Sides, item.Face2Sides, item.Either, f);
+            poly.DebugVerts = poly.Vertices.Select(v => v.Position).ToList();
+        }
         return poly;
     }
 }
